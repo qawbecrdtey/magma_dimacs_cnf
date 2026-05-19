e2bb737099f0586b3b6267996890c94264bebccc
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2026-05-19 11:02:54 +0900

    Working on it.

--- include/magma_dimacs_cnf/create_cnf.hpp
+++ include/magma_dimacs_cnf/create_cnf.hpp
@@ -10,10 +10,76 @@
 #include <vector>
 
 namespace magma_dimacs_cnf {
+    static constexpr void at_least_one(
+      std::vector<std::int32_t> const &literals_,
+      std::vector<std::vector<std::int32_t>> &cnf_,
+      std::int32_t &next_,
+      std::int32_t const group_size_ = 3) {
+        ASSERT_AND_ASSUME(2 < group_size_);
+
+        auto const n = static_cast<std::int32_t>(literals_.size());
+        if(n < 2 * group_size_ - 2) {
+            cnf_.emplace_back(literals_);
+            return;
+        }
+
+        std::int32_t idx = 0;
+        std::vector<std::int32_t> next_literals;
+        while(idx < n) {
+            auto const len = group_size_ + ((n - idx) % (group_size_ - 1) != 0);
+            std::vector<std::int32_t> cl;
+            cl.reserve(len);
+            cl.emplace_back(-next_);
+            next_literals.emplace_back(next_);
+            for(std::int32_t i = 1; i < len; i++, idx++) {
+                cl.emplace_back(literals_[idx]);
+                cnf_.emplace_back(std::initializer_list {next_, -literals_[idx]});
+            }
+            cnf_.emplace_back(std::move(cl));
+            next_++;
+        }
+
+        at_least_one(next_literals, cnf_, next_, group_size_);
+    }
+
+    [[maybe_unused]]
+    static constexpr void lexicographically_less(
+      std::vector<std::int32_t> const &lhs_,
+      std::vector<std::int32_t> const &rhs_,
+      std::vector<std::vector<std::int32_t>> &cnf_,
+      std::int32_t &next_) {
+        ASSERT_AND_ASSUME(lhs_.size() == rhs_.size());
+
+        cnf_.emplace_back(std::initializer_list {next_});
+        auto const n = static_cast<std::int32_t>(lhs_.size());
+        std::vector<std::int32_t> cl;
+        cl.reserve(n);
+        for(std::int32_t i = 0; i < n; i++) {
+            auto const now1 = next_++;
+            auto const now2 = next_++;
+
+            cnf_.emplace_back(std::initializer_list {-next_, now1});
+            cnf_.emplace_back(std::initializer_list {-next_, lhs_[i], -rhs_[i]});
+            cnf_.emplace_back(std::initializer_list {-next_, -lhs_[i], rhs_[i]});
+            cnf_.emplace_back(std::initializer_list {-now1, -lhs_[i], rhs_[i], next_});
+            cnf_.emplace_back(std::initializer_list {-now1, lhs_[i], -rhs_[i], next_});
+
+            cnf_.emplace_back(std::initializer_list {-now2, now1});
+            cnf_.emplace_back(std::initializer_list {-now2, -lhs_[i]});
+            cnf_.emplace_back(std::initializer_list {-now2, rhs_[i]});
+            cnf_.emplace_back(std::initializer_list {-now1, lhs_[i], -rhs_[i], now2});
+
+            cl.emplace_back(now2);
+        }
+
+        next_++;
+        at_least_one(cl, cnf_, next_);
+    }
+
     static constexpr void create_cnf_magma(
       input_flags const *const flags_,
       std::vector<std::vector<std::int32_t>> &cnf_,
-      [[maybe_unused]] std::int32_t &next_) {
+      std::int32_t &next_) {
         auto const n = flags_->element_count;
         ASSERT_AND_ASSUME(0 < n);
         CREATE_VAR(n);
@@ -29,7 +95,7 @@ namespace magma_dimacs_cnf {
                 std::vector<std::int32_t> cl;
                 cl.reserve(n);
                 for(std::int32_t k = 0; k < n; k++) { cl.emplace_back(var(i, j, k)); }
-                cnf_.emplace_back(std::move(cl));
+                at_least_one(cl, cnf_, next_);
             }
         }
     }
@@ -37,7 +103,7 @@ namespace magma_dimacs_cnf {
     static constexpr void create_cnf_inv_magma(
       input_flags const *const flags_,
       std::vector<std::vector<std::int32_t>> &cnf_,
-      [[maybe_unused]] std::int32_t &next_) {
+      std::int32_t &next_) {
         auto const n = flags_->element_count;
         ASSERT_AND_ASSUME(0 < n);
         CREATE_INV_VAR(n);
@@ -54,7 +120,7 @@ namespace magma_dimacs_cnf {
                 std::vector<std::int32_t> cl;
                 cl.reserve(n);
                 for(std::int32_t k = 0; k < n; k++) { cl.emplace_back(inv_var(i, j, k)); }
-                cnf_.emplace_back(std::move(cl));
+                at_least_one(cl, cnf_, next_);
             }
         }
     }
@@ -67,6 +133,7 @@ namespace magma_dimacs_cnf {
         ASSERT_AND_ASSUME(0 < n);
         CREATE_VAR(n);
 
+        // $x = y(x((yx)y))$
         for(std::int32_t i = 0; i < n; i++) {  // x
             for(std::int32_t j = 0; j < n; j++) {  // y
                 for(std::int32_t k = 0; k < n; k++) {  // yx
@@ -90,6 +157,7 @@ namespace magma_dimacs_cnf {
         ASSERT_AND_ASSUME(0 < n);
         CREATE_INV_VAR(n);
 
+        // $x = (((y*x)*x)*(y*x))*y$
         for(std::int32_t i = 0; i < n; i++) {  // x
             for(std::int32_t j = 0; j < n; j++) {  // y
                 for(std::int32_t k = 0; k < n; k++) {  // y*x
@@ -122,6 +190,26 @@ namespace magma_dimacs_cnf {
                 for(std::int32_t k = 0; k < n; k++) {  // z
                     cnf_.emplace_back(std::initializer_list {-var(i, j, k), inv_var(k, i, j)});
                     cnf_.emplace_back(std::initializer_list {var(i, j, k), -inv_var(k, i, j)});
+                }
+            }
+        }
+
+        // For every $x$ and $y$, $x*y=x((yx)y)$ holds.
+        for(std::int32_t i = 0; i < n; i++) {  // x
+            for(std::int32_t j = 0; j < n; j++) {  // y
+                for(std::int32_t k = 0; k < n; k++) {  // yx
+                    for(std::int32_t l = 0; l < n; l++) {  // (yx)y
+                        for(std::int32_t m = 0; m < n; m++) {  // (1) x((yx)y)  // (2) x*y
+                            // (1)
+                            cnf_.emplace_back(
+                              std::initializer_list {
+                                -var(j, i, k), -var(k, j, l), -var(i, l, m), inv_var(i, j, m)});
+                            // (2)
+                            cnf_.emplace_back(
+                              std::initializer_list {
+                                -var(j, i, k), -var(k, j, l), -inv_var(i, j, m), var(i, l, m)});
+                        }
+                    }
                 }
             }
         }
@@ -173,7 +261,7 @@ namespace magma_dimacs_cnf {
         // For any $x$, $(xx)x=(x((xx)x))(xx)=(x*x)(xx)$ holds.
         for(std::int32_t i = 0; i < n; i++) {  // x
             for(std::int32_t j = 0; j < n; j++) {  // xx
-                for(std::int32_t k = 0; k < n; k++) {  //(xx)x
+                for(std::int32_t k = 0; k < n; k++) {  // (xx)x
                     for(std::int32_t l = 0; l < n; l++) {  // (1) x((xx)x)  // (2) x*x
                         // (1)
                         cnf_.emplace_back(
@@ -294,11 +382,9 @@ namespace magma_dimacs_cnf {
     static constexpr void create_cnf_677_properties(
       input_flags const *const flags_,
       std::vector<std::vector<std::int32_t>> &cnf_,
-      [[maybe_unused]] std::int32_t &next_) {
+      std::int32_t &next_) {
         auto const n = flags_->element_count;
         ASSERT_AND_ASSUME(0 < n);
-        // CREATE_VAR(n);
-        // CREATE_INV_VAR(n);
 
         create_cnf_677_general_properties(flags_, cnf_, next_);
         create_cnf_677_idempotent_properties(flags_, cnf_, next_);
@@ -307,7 +393,8 @@ namespace magma_dimacs_cnf {
     static constexpr void create_cnf_first_line_constraint(
       input_flags const *const flags_,
       std::vector<std::int32_t> const &v_,
-      std::vector<std::vector<std::int32_t>> &cnf_) {
+      std::vector<std::vector<std::int32_t>> &cnf_,
+      [[maybe_unused]] std::int32_t &next_) {
         auto const n = flags_->element_count;
         ASSERT_AND_ASSUME(sum(v_) == n);
         CREATE_VAR(n);
@@ -343,17 +430,35 @@ namespace magma_dimacs_cnf {
 
     static constexpr void create_cnf_677_symmetry_breaker(
       input_flags const *const flags_,
-      [[maybe_unused]] std::vector<std::int32_t> const &v_,
+      std::vector<std::int32_t> const &v_,
       std::vector<std::vector<std::int32_t>> &cnf_,
-      [[maybe_unused]] std::int32_t &next_) {
-        create_cnf_first_line_constraint(flags_, v_, cnf_);
+      std::int32_t &next_) {
+        create_cnf_first_line_constraint(flags_, v_, cnf_, next_);
+    }
+
+    static constexpr void create_cnf_677_not_255(
+      input_flags const *const flags_,
+      std::vector<std::vector<std::int32_t>> &cnf_,
+      std::int32_t const not_255_) {
+        auto const n = flags_->element_count;
+        ASSERT_AND_ASSUME(0 < n);
+        CREATE_VAR(n);
+
+        for(std::int32_t i = 0; i < n; i++) {  // xx
+            for(std::int32_t j = 0; j < n; j++) {  // (xx)x
+                cnf_.emplace_back(
+                  std::initializer_list {
+                    -var(not_255_, not_255_, i), -var(i, not_255_, j), -var(j, not_255_, not_255_)});
+            }
+        }
     }
 
     static constexpr void create_cnf(
       input_flags const *const flags_,
       std::vector<std::int32_t> const &v_,
       std::vector<std::vector<std::int32_t>> &cnf_,
-      [[maybe_unused]] std::int32_t &next_) {
+      std::int32_t &next_,
+      std::int32_t const not_255_ = -1) {
         create_cnf_magma(flags_, cnf_, next_);
         create_cnf_inv_magma(flags_, cnf_, next_);
         create_cnf_677_magma(flags_, cnf_, next_);
@@ -361,6 +466,8 @@ namespace magma_dimacs_cnf {
         create_cnf_677_equivalence(flags_, cnf_, next_);
         create_cnf_677_properties(flags_, cnf_, next_);
         create_cnf_677_symmetry_breaker(flags_, v_, cnf_, next_);
+
+        if(not_255_ != -1) { create_cnf_677_not_255(flags_, cnf_, not_255_); }
     }
 }  // namespace magma_dimacs_cnf
 
